fd2b8c311db598626aae60108af64a492a5f0e5e
teukusulthan <teukusultanul@gmail.com>
2025-08-07 17:34:57 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("Decrease");
+const reset = document.getElementById("Reset");
+const number = document.getElementById("Number");
+
+incerease.onclick = function () {};
