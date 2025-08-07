deb52b6d1ac1aa4cbfc318019f60767d051d3b32
teukusulthan <teukusultanul@gmail.com>
2025-08-07 19:04:13 +0700

    feat: add click sound effect to counter buttons

--- index.js
+++ index.js
@@ -3,18 +3,29 @@ const decrease = document.getElementById("decrease");
 const reset = document.getElementById("reset");
 const countP = document.getElementById("count");
 let count = 0;
+const clickSound = new Audio("sound/btn.mp3");
+const resetSound = new Audio("sound/reset.mp3");
+
+function playClickSound() {
+  clickSound.currentTime = 0;
+  clickSound.play();
+}
 
 increase.onclick = function () {
+  clickSound.play();
   count++;
   countP.textContent = count;
+  pla;
 };
 
 decrease.onclick = function () {
+  clickSound.play();
   count--;
   countP.textContent = count;
 };
 
 reset.onclick = function () {
+  resetSound.play();
   count = 0;
   countP.textContent = count;
 };
